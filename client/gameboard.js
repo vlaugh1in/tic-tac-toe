908898c7c3944a639f08a383ccbaa2133982e95a
Victoria Laughlin <victoria.laughlin@gmail.com>
2023-08-21 20:15:42 -0400

    init
    setup general look of app

---
+++ client/gameboard.js
@@ -0,0 +1,28 @@
+
+//revealing module (lets you reveal api in return)
+
+var gameboard = (function(){
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+ //cache dom
+  
+
+  //bind events
+ 
+
+  _render();
+
+  function _render() {
+    
+  }
+
+
+
+  return {
+    
+  };
+
+})();
+
+
+
---
+++ client/stats.js
@@ -0,0 +1,9 @@
+
+
+var stats = (function(){
+ 
+})();
+
+
+
+
